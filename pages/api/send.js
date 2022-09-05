9e40d1c5d03ac3f67ee9d90ad6468f37054d25fd
mu-tech8 <magobe.theword@gmail.com>
2022-09-05 23:52:50 +0900

    mail setting

--- pages/api/send.js
+++ pages/api/send.js
@@ -6,7 +6,7 @@ export default function handler(req, res) {
     sgMail.setApiKey(process.env.SENDGRID_APIKEY);
     const msg = {
       to: req.body.email,
-      bcc: "mutech8@gmail.com",
+      bcc: "24800mm@gmail.com",
       from: "info_satohonatsu@248cm.com",
       subject: "お問合せありがとうございました。",
       text: `${req.body.name} 様\nお問合せを受け付けました。回答をお待ちください。\n\n ${req.body.message}`,
