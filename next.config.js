cda53daefcacc95eae9453884524c6bed0dc1ec6
mu-tech8 <magobe.theword@gmail.com>
2022-09-09 17:24:23 +0900

    change key

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   reactStrictMode: false,
   env: {
-    NEXT_PUBLIC_SEND_GRID_API_KEY: process.env.NEXT_PUBLIC_SEND_GRID_API_KEY,
+    SENDGRID_API_KEY: process.env.NEXT_PUBLIC_SEND_GRID_API_KEY,
     API_KEY: process.env.API_KEY,
   },
 };
