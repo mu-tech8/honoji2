a390c420be0d5beabf94f56add9b5bedd47018c5
mu-tech8 <magobe.theword@gmail.com>
2022-09-14 19:59:12 +0900

    modify config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   reactStrictMode: false,
   env: {
-    SENDGRID_API_KEY: process.env.NEXT_PUBLIC_SEND_GRID_API_KEY,
+    SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
     API_KEY: process.env.API_KEY,
     BCC_MAIL: process.env.BCC_MAIL,
     FROM_MAIL: process.env.FROM_MAIL,
