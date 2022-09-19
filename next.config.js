a15060947c28f54471f87a8cbf174b4b85d05703
mu-tech8 <magobe.theword@gmail.com>
2022-09-19 18:12:09 +0900

    try congig

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   reactStrictMode: false,
+  env: {
+    SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
+    API_KEY: process.env.API_KEY,
+    BCC_MAIL: process.env.BCC_MAIL,
+    FROM_MAIL: process.env.FROM_MAIL,
+  },
 };
