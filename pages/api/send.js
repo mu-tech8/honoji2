ff0bd5316b5b7b9288b8261e635237b9e6210e06
mu-tech8 <magobe.theword@gmail.com>
2022-09-19 16:44:35 +0900

    try catch error

--- pages/api/send.js
+++ pages/api/send.js
@@ -20,6 +20,7 @@ export default function handler(req, res) {
         if (error.response) {
           console.error(error.response.body);
         }
+        res.status(400).json({ status: "ERROR", message: error.message });
       }
     })();
   }
